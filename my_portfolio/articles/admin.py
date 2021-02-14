a3a51ff921317dfb5b64516d9839f6b068e12402
Krzyzaku21 <krzyzak21@gmail.com>
2021-02-14 03:35:20 +0100

    responsiveness website

--- my_portfolio/articles/admin.py
+++ my_portfolio/articles/admin.py
@@ -5,8 +5,3 @@ from mptt.admin import MPTTModelAdmin
 admin.site.register(models.Comment, MPTTModelAdmin)
 
 admin.site.register(models.Article)
-
-
-# @admin.register(models.Post)
-# class AuthorAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'id', 'status')
